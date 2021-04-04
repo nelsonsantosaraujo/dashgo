386d817ab56d7bc4933515e2cda000a71a88ba48
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-04-04 13:24:36 -0300

    Sidebar responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
 
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion  = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,18 @@ export function Header() {
       px="6"
       align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
       <Logo />
 
       { isWideVersion && (<SearchBox />) }
