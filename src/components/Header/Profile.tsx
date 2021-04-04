b921e52a193599f5481b7cbf415a95f10afbf4a6
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-04-04 13:03:08 -0300

    Header responsiveness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Nelson Araújo</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          nelson@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Nelson Araújo</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            nelson@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Nelson Araújo" src="https://github.com/nelsonsantosaraujo.png" />          
     </Flex>
