a75867f0526b676323e8c5c228b0402c79df492e
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-04-11 13:45:14 -0300

    User list with react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,18 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }));
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -67,35 +78,37 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Nelson Araújo</Text>
-                        <Text fontSize="small" color="gray.300">nelson@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && (
-                      <Td>
-                        04 de Abril, 2021
+                  { data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
                       </Td>
-                    )}
-                    { isWideVersion && (
                       <Td>
-                        <Button
-                          as="a"
-                          size="sm"
-                          fontSize="small"
-                          colorScheme="pink"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                        >
-                          Editar
-                        </Button>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="small" color="gray.300">{user.email}</Text>
+                        </Box>
                       </Td>
-                    )}
-                  </Tr>
+                      { isWideVersion && (
+                        <Td>
+                          {user.createdAt}
+                        </Td>
+                      )}
+                      { isWideVersion && (
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="small"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  )) }
                 </Tbody>
               </Table>
 
